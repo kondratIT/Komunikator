1f1c5c7928f82084fc50f42b3f8fe546772282b8
kondratIT <it.kondrat@gmail.com>
2019-05-06 08:26:25 +0200

    Create claen ReciveMesageActivity

---
+++ app/src/main/java/kondrat/pl/komunikator/ReciveMessageActivity.java
@@ -0,0 +1,13 @@
+package kondrat.pl.komunikator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReciveMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recive_message);
+    }
+}
