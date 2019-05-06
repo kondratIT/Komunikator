52f56af7a8eb255bffa0c6665c205494fd94b864
kondratIT <it.kondrat@gmail.com>
2019-05-06 09:29:03 +0200

    Create intent to ReciveMessageActivity

--- app/src/main/java/kondrat/pl/komunikator/CreateMessageActivity.java
+++ app/src/main/java/kondrat/pl/komunikator/CreateMessageActivity.java
@@ -1,8 +1,11 @@
 package kondrat.pl.komunikator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.EditText;
 
 public class CreateMessageActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class CreateMessageActivity extends AppCompatActivity {
     }
 
     public void onSendMessage(View view){
-
+        
+        Intent intent = new Intent(this, ReciveMessageActivity.class);
+        startActivity(intent);
     }
 }
