189ec7d724818dc80165a68b717e72e8761770da
kondratIT <it.kondrat@gmail.com>
2019-05-06 07:58:56 +0200

    Firsst initial commit

---
+++ app/src/main/java/kondrat/pl/komunikator/CreateMessageActivity.java
@@ -0,0 +1,18 @@
+package kondrat.pl.komunikator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_message);
+    }
+
+    public void onSendMessage(View view){
+
+    }
+}
