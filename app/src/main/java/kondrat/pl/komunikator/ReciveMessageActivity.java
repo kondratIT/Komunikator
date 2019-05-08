79512961aacd1d35c432b015ef775923000d7dde
kondratIT <it.kondrat@gmail.com>
2019-05-08 09:17:18 +0200

    Handling message

--- app/src/main/java/kondrat/pl/komunikator/ReciveMessageActivity.java
+++ app/src/main/java/kondrat/pl/komunikator/ReciveMessageActivity.java
@@ -1,7 +1,10 @@
 package kondrat.pl.komunikator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.TextView;
 
 public class ReciveMessageActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE="message";
@@ -10,5 +13,12 @@ public class ReciveMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recive_message);
+        Intent intent = getIntent();
+        String messageText = intent.getStringExtra(EXTRA_MESSAGE);
+        TextView messageView = findViewById(R.id.message);
+        messageView.setText(messageText);
+
+
+
     }
 }
